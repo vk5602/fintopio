6c2c3b86fec8cfbbce5c87a99f4307d675f5b4c3
vk5602 <72371093+vk5602@users.noreply.github.com>
2024-11-24 21:57:34 +0530

    Add files via upload

---
+++ config/banner.js
@@ -0,0 +1,28 @@
+function printBanner() {
+    console.log("╔══════════════════════════════════╗");
+    console.log("║                                  ║");
+    console.log("║          ZAIN ARAIN              ║");
+    console.log("║                                  ║");
+    console.log("║  JOIN TELEGRAM CHANNEL FOR LATEST║");
+    console.log("║  UPDATES & SUPPORT!              ║");
+    console.log("║  (https://t.me/AirdropScript)    ║");
+    console.log("║                                  ║");
+    console.log("║  @AirdropScript6 - OFFICIAL      ║");
+    console.log("║          CHANNEL                 ║");
+    console.log("║                                  ║");
+    console.log("╚══════════════════════════════════╝");
+
+    console.log("──────────────────────────────────────");
+    console.log("|                                    |");
+    console.log("|        AUTO SCRIPT Script...       |");
+    console.log("|                                    |");
+    console.log("|             🔥🔥                  |");
+    console.log("|            Zain Arain              |");
+    console.log("|                                    |");
+    console.log("──────────────────────────────────────");
+
+    console.log("Telegram: https://t.me/AirdropScript6");
+    console.log("=================================");
+}
+
+module.exports = printBanner;
